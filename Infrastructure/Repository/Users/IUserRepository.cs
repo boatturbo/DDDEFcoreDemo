b2babc317425eaecb1dcb3e5f89614d16c88c3af
chenguodong <1441491126@qq.com>
2022-10-09 14:05:15 +0800

    IUserRepository继承IRepositoryBase，添加待实现方法GetAllUser；

--- Infrastructure/Repository/Users/IUserRepository.cs
+++ Infrastructure/Repository/Users/IUserRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Infrastructure.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,8 @@ using System.Threading.Tasks;
 
 namespace Infrastructure.Repository.Users
 {
-    public interface IUserRepository
+    public interface IUserRepository : IRepositoryBase
     {
+        public List<User> GetAllUser();
     }
 }
